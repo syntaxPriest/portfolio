9cbebc4b498feacdcc511172ef727b5c7f075434
Daniel Adewale <adewaled03@gmail.com>
2023-03-06 20:31:41 +0100

    Update banner.js

--- src/components/banner.js
+++ src/components/banner.js
@@ -20,7 +20,7 @@ const Banner = () => {
                 <div className="banner flex centralized-v"> 
                     <div className="banner-text">
                         <h4>Hi, I'm Daniel Priestley Adewale ☕</h4>
-                        <h1>Software Engineer, Front End Developer, UI/UX Designer</h1>
+                        <h1>Software Engineer, Front End Developer, Mobile Developer</h1>
                         
                         <a href="DanielResume.pdf" download>
                             <button>Download Resume</button>
